46a455d66b9f81ead1a003f02eb6a6712b103051
aknarang03 <s1328134@monmouth.edu>
2023-12-09 16:57:41 -0500

    sylvan can collect tokens while possessing an enemy now

--- core/src/com/mygdx/game/GameContactListener.java
+++ core/src/com/mygdx/game/GameContactListener.java
@@ -18,16 +18,16 @@ public class GameContactListener implements ContactListener {
     @Override
     public void beginContact(Contact contact) {
 
-        // figure out which fixture is enemy and which one is sylvan
-        Fixture other = (contact.getFixtureA().getBody().getUserData() == "sylvan") ? contact.getFixtureB() : contact.getFixtureA();
-        Fixture sylvan = (contact.getFixtureA().getBody().getUserData() == "sylvan") ? contact.getFixtureA() : contact.getFixtureB();
+        // figure out which fixture is enemy and which one is player
+        Fixture other = (contact.getFixtureA().getBody().getUserData() == level.currentInhabitedEntity.getBody().getUserData()) ? contact.getFixtureB() : contact.getFixtureA();
+        Fixture player = (contact.getFixtureA().getBody().getUserData() == level.currentInhabitedEntity.getBody().getUserData()) ? contact.getFixtureA() : contact.getFixtureB();
 
         // KNOCKBACK
 
-        if (sylvan.getBody().getUserData() == "sylvan" && (other.getBody().getUserData() == "bat" || other.getBody().getUserData() == "spider")) {
+        if (player.getBody().getUserData() == "sylvan" && (other.getBody().getUserData() == "bat" || other.getBody().getUserData() == "spider")) {
 
-            double velx = sylvan.getBody().getLinearVelocity().x;
-            double vely = sylvan.getBody().getLinearVelocity().y;
+            double velx = player.getBody().getLinearVelocity().x;
+            double vely = player.getBody().getLinearVelocity().y;
 
             boolean left = (velx < 0);
             boolean right = (velx > 0);
@@ -40,25 +40,24 @@ public class GameContactListener implements ContactListener {
             final float FORCELEFT = -0.8f;
 
             if (left && up) {
-                sylvan.getBody().applyForceToCenter(FORCERIGHT,FORCEDOWN,true);
+                player.getBody().applyForceToCenter(FORCERIGHT,FORCEDOWN,true);
             } else if (right && up) {
-                sylvan.getBody().applyForceToCenter(FORCELEFT,FORCEDOWN,true);
+                player.getBody().applyForceToCenter(FORCELEFT,FORCEDOWN,true);
             } else if (up) {
-                sylvan.getBody().applyForceToCenter(0,FORCEDOWN,true);
+                player.getBody().applyForceToCenter(0,FORCEDOWN,true);
             } else if (left && down) {
-                sylvan.getBody().applyForceToCenter(FORCERIGHT,FORCEUP,true);
+                player.getBody().applyForceToCenter(FORCERIGHT,FORCEUP,true);
             } else if (right && down) {
-                sylvan.getBody().applyForceToCenter(FORCELEFT,FORCEUP,true);
+                player.getBody().applyForceToCenter(FORCELEFT,FORCEUP,true);
             } else if (down) {
-                sylvan.getBody().applyForceToCenter(0,FORCEUP,true);
+                player.getBody().applyForceToCenter(0,FORCEUP,true);
             } else if (left) {
-                sylvan.getBody().applyForceToCenter(FORCERIGHT,0,true);
+                player.getBody().applyForceToCenter(FORCERIGHT,0,true);
             } else if (right) {
-                sylvan.getBody().applyForceToCenter(FORCELEFT,0,true);
+                player.getBody().applyForceToCenter(FORCELEFT,0,true);
             }
 
             level.sylvan.takeDamage();
-            //level.sylvan.currentState = Entity.State.HIT;
 
         }
 
